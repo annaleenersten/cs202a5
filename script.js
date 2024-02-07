8076ec7e9a22e0691a2edb7a54439d63588e3d91
annaleenersten <121985630+annaleenersten@users.noreply.github.com>
2024-02-07 13:46:03 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function displayNumber(){
+    document.getElementById("number1").innerHTML = "Move forward " + randNumber() + " steps";
+    document.getElementById("number2").innerHTML = "Move forward " + randNumber() + " steps";
+    document.getElementById("number3").innerHTML = "Move back " + randNumber() + " steps";
+    document.getElementById("number4").innerHTML = "Move forward " + randNumber() + " steps";
+    document.getElementById("number5").innerHTML = "Move forward " + randNumber() + " steps";
+    document.getElementById("number6").innerHTML = "Move forward " + randNumber() + " steps";
+    document.getElementById("number7").innerHTML = "Move back " + randNumber() + " steps";
+    document.getElementById("number8").innerHTML = "Move forward " + randNumber() + " steps";
+    document.getElementById("number9").innerHTML = "Move forward " + randNumber() + " steps";
+    document.getElementById("number10").innerHTML = "Move forward " + randNumber() + " steps";
+    document.getElementById("number11").innerHTML = "Move back " + randNumber() + " steps";
+    document.getElementById("number12").innerHTML = "Move forward " + randNumber() + " steps";
+}
+
+function randNumber(){
+    return Math.floor(Math.random()*15) + 1;
+}
