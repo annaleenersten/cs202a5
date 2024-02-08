6e7d27c34c927ac8aecd92971f48185135410fb9
annaleenersten <121985630+annaleenersten@users.noreply.github.com>
2024-02-07 21:37:44 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function displayNumber(){
-    document.getElementById("number1").innerHTML = "Move forward " + randNumber() + " steps";
+    //document.getElementById("number1").innerHTML = "Move forward " + randNumber() + " steps";
     document.getElementById("number2").innerHTML = "Move forward " + randNumber() + " steps";
-    document.getElementById("number3").innerHTML = "Move back " + randNumber() + " steps";
-    document.getElementById("number4").innerHTML = "Move forward " + randNumber() + " steps";
-    document.getElementById("number5").innerHTML = "Move forward " + randNumber() + " steps";
+   // document.getElementById("number3").innerHTML = "Move back " + randNumber() + " steps";
+    document.getElementById("number4").innerHTML = "Move back " + randNumber2() + " steps";
+   // document.getElementById("number5").innerHTML = "Move forward " + randNumber() + " steps";
     document.getElementById("number6").innerHTML = "Move forward " + randNumber() + " steps";
-    document.getElementById("number7").innerHTML = "Move back " + randNumber() + " steps";
+    //document.getElementById("number7").innerHTML = "Move back " + randNumber() + " steps";
     document.getElementById("number8").innerHTML = "Move forward " + randNumber() + " steps";
-    document.getElementById("number9").innerHTML = "Move forward " + randNumber() + " steps";
+   // document.getElementById("number9").innerHTML = "Move forward " + randNumber() + " steps";
     document.getElementById("number10").innerHTML = "Move forward " + randNumber() + " steps";
-    document.getElementById("number11").innerHTML = "Move back " + randNumber() + " steps";
-    document.getElementById("number12").innerHTML = "Move forward " + randNumber() + " steps";
+    //document.getElementById("number11").innerHTML = "Move back " + randNumber() + " steps";
+    document.getElementById("number12").innerHTML = "Move back " + randNumber2() + " steps";
 }
 
 function randNumber(){
     return Math.floor(Math.random()*14) + 2;
+}
+
+function randNumber2(){
+    return Math.floor(Math.random()*5) + 2;
 }
