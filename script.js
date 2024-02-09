c6c9922fb698c8f770cd4264b59beedf9ec1d5b6
annaleenersten <121985630+annaleenersten@users.noreply.github.com>
2024-02-08 16:24:13 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 function displayNumber(){
-    //document.getElementById("number1").innerHTML = "Move forward " + randNumber() + " steps";
     document.getElementById("number2").innerHTML = "Move forward " + randNumber() + " steps";
-   // document.getElementById("number3").innerHTML = "Move back " + randNumber() + " steps";
     document.getElementById("number4").innerHTML = "Move back " + randNumber2() + " steps";
-   // document.getElementById("number5").innerHTML = "Move forward " + randNumber() + " steps";
     document.getElementById("number6").innerHTML = "Move forward " + randNumber() + " steps";
-    //document.getElementById("number7").innerHTML = "Move back " + randNumber() + " steps";
     document.getElementById("number8").innerHTML = "Move forward " + randNumber() + " steps";
-   // document.getElementById("number9").innerHTML = "Move forward " + randNumber() + " steps";
     document.getElementById("number10").innerHTML = "Move forward " + randNumber() + " steps";
-    //document.getElementById("number11").innerHTML = "Move back " + randNumber() + " steps";
     document.getElementById("number12").innerHTML = "Move back " + randNumber2() + " steps";
 }
 
